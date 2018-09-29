be5cf948ac757a5f0e7c3cb3ed8ee64c06f46e5d
Aleksandra <aleksgalitskaya@gmail.com>
2018-09-29 15:02:51 +0300

    Добавлены 2 файла

--- src/com/idea/HelloWorld1.java
+++ src/com/idea/HelloWorld1.java
@@ -2,6 +2,6 @@ package com.idea;
 
 public class HelloWorld1 {
     public static void main(String[] args) {
-        System.out.println("Hello, world 1!");
+        System.out.println("Hello, world 11!");
     }
 }
